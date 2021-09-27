fabd713d5616f9b408ab5b9abbd710acc375c010
deekshadiku <91430571+deekshadiku@users.noreply.github.com>
2021-09-27 11:16:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+	WEB 303
+	Starting file for Assignment 1 
+	{Your Name Here}
+*/
+
+function myFunction() {
+
+			var x= document.getElementById("salary").value;
+			var y = document.getElementById("percent").value;
+			var z= x*y/100;
+			document.getElementById("spend").innerHTML = z;
+		}
